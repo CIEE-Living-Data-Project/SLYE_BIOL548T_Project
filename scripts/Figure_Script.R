ce7d82b20229c0eda3a76cab8e8e47daec0ccfb4
Sadie Lye <sadielye@student.ubc.ca>
2022-09-22 14:19:33 -0700

    Idk trying to fix the differences

---
+++ scripts/Figure_Script.R
@@ -0,0 +1,33 @@
+#Created: September 22nd, 2022
+#Owner: Sade Lye
+# This Script creates figures for the manuscript
+
+#load packages
+library("tidyverse")
+library("ggplot2")
+library("cowplot")
+
+#set theme
+theme_set(theme_cowplot ())
+
+#load the data
+
+class <- read_csv("data/processed/NRCA_1995_2005_TLW_invertclassification_cleaned.csv")
+
+density <- read_csv("data/processed/NRCA_1995_2005_TLW_invertdensity_cleaned.csv")
+
+#bar plot to show the percentage of the total at which level we have taxonomic information for
+
+bar <- ggplot(data = class, aes (x = level, y = count_percentoftotal)) + geom_bar(stat = "identity")
+bar
+
+# Making a scatterplot for the manuscript
+
+scatter <- ggplot(data = density, aes ( x= month, y = abundance, colour = species )) + geom_point() + facet_wrap(~year)
+scatter
+
+ggsave(bar, "figures/classification.png")
+
+
+########### End of Script ##################
+
